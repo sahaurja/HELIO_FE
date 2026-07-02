2c3f391fc389db094132de5ba7c1071a9be58329
Urja Saha <us64@cornell.edu>
2026-07-02 19:39:11 -0400

    folder structure and path

--- flip-fe/src/App.jsx
+++ flip-fe/src/App.jsx
@@ -1,12 +1,15 @@
-
 import './App.css'
+import FlashcardView from './pages/FlashcardView'
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      
+      <Router>
+          <Routes>
+              <Route path = "/flashcards" element = {<FlashcardView/>}/>
+          </Routes>
+      </Router>
     </>
   )
 }
---
+++ flip-fe/src/pages/FlashcardView.jsx
@@ -0,0 +1,7 @@
+export default function FlashcardView(){
+    return(
+        <>
+            <h1>View Flashcards Here</h1>
+        </>
+    )
+}
