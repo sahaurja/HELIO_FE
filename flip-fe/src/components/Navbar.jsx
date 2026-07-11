23beddbb7e6dcb0c368fb2de6b39c45a86dd33e7
Urja Saha <us64@cornell.edu>
2026-07-10 23:02:25 -0400

    added translate to nav

--- flip-fe/src/components/Navbar.jsx
+++ flip-fe/src/components/Navbar.jsx
@@ -5,6 +5,8 @@ export default function Navbar() {
                 <a href = "/">Home</a>
                 <br/>
                 <a href = "/flashcards">Flashcards</a>
+                <br/>
+                <a href = "/translate">Translator</a>
             </nav>
         </>
     )
