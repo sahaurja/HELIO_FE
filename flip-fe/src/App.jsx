e9714d6ce52549a6a477304401abaac9ec55b021
Urja Saha <us64@cornell.edu>
2026-08-30 00:14:09 -0400

    use nav

--- flip-fe/src/App.jsx
+++ flip-fe/src/App.jsx
@@ -3,7 +3,7 @@ import Flashcard from './components/Flashcard'
 import Home from './pages/Home_Sections/Home'
 import Navbar from './components/Navbar'
 import Translator from './pages/Translator'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom'
 import Login from './pages/Login'
 import { ToastContainer, toast } from 'react-toastify';
 import RealNavbar from './components/RealNavbar'
@@ -16,6 +16,7 @@ import FlashcardView from './pages/FlashcardView'
 
 function App() {
     const [loggedIn, setLoggedIn] = useState(false)
+    const navigate = useNavigate()
     const BACKEND_URL = "https://helio-be.vercel.app";
 
     //check if alr logged in, else redirect 
